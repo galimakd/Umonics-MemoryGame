0d9ec9f3e814bfdf2ef44ff2b0c2593e1d0f976c
Justine Lo Belza <JustineLobelza@gmail.com>
2022-08-03 16:28:24 +0800

    Volume Fixed

--- Umonics - Memory Game/Assets/Scripts/VolumeHandler.cs
+++ Umonics - Memory Game/Assets/Scripts/VolumeHandler.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 public class VolumeHandler : MonoBehaviour
 {
     [SerializeField] private AudioSource[] sources;
+    private float[] initValues;
 
     // Start is called before the first frame update
     void Start()
     {
+        initValues = new float[sources.Length];
+        for(int i = 0; i < sources.Length; i++){
+            initValues[i] = sources[i].volume;
+        }
         ManualUpdate();
     }
 
     public void ManualUpdate(){
         VolumeSetting volumeSetting = VolumeSetting.GetInstance();
 
-        foreach(AudioSource source in sources){
-            source.volume *= volumeSetting.Volume;
+        for(int i = 0; i < sources.Length; i++){
+            sources[i].volume = initValues[i] * volumeSetting.Volume;
         }
     }
 }
