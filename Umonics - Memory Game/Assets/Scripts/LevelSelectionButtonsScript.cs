05a7383669d3e53193dd495a55f68f4d6bb89503
Justine Lo Belza <JustineLobelza@gmail.com>
2022-08-12 14:55:08 +0800

    Doors' Locks animation added

--- Umonics - Memory Game/Assets/Scripts/LevelSelectionButtonsScript.cs
+++ Umonics - Memory Game/Assets/Scripts/LevelSelectionButtonsScript.cs
@@ -8,21 +8,42 @@ public class LevelSelectionButtonsScript : MonoBehaviour
     [SerializeField] private AudioClip doorKnockClip, backClip;
     private LevelSelectionDB levelSelectionDB;
     [SerializeField] private GameObject mediumLock, hardLock;
+    [SerializeField] private Animator mediumAnimator, hardAnimator;
     
     enum Difficulty{EASY, MEDIUM, HARD}
 
+    private static bool isMedLock = true, isHardLock = true;
 
     private void Start() {
         levelSelectionDB = LevelSelectionDB.GetInstance();
 
         // Lock thingy
-        if(levelSelectionDB.LevelUnlocked >= 2){
+        if(isMedLock){
+            if(levelSelectionDB.LevelUnlocked >= 2){ //unlocking
+                isMedLock = false;
+                mediumAnimator.Play("DoorLock_Unlocking", 0, 0);
+                StartCoroutine(LockSetActiveFalse(mediumLock));
+            }
+        }else{ //already unlocked
             mediumLock.SetActive(false);
         }
 
-         if(levelSelectionDB.LevelUnlocked == 3){
+        if(isHardLock){
+            if(levelSelectionDB.LevelUnlocked == 3){ //unlocking
+                isHardLock = false;
+                hardAnimator.Play("DoorLock_Unlocking", 0, 0);
+                StartCoroutine(LockSetActiveFalse(hardLock));
+            }
+        }else{ //already unlocked
             hardLock.SetActive(false);
         }
+
+        
+    }
+
+    IEnumerator LockSetActiveFalse(GameObject lockToDisable){
+        yield return new WaitForSeconds(1.4f);
+        lockToDisable.SetActive(false);
     }
 
     public void BackClicked(){
